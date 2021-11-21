13669e01ab6effbd18fdb54ebb8b18bfcbf36c64
Mihail Gordovskii <hordovskyimisha@gmail.com>
2021-11-21 17:07:19 +0200

    Style path fixed

---
+++ dev_data_test/js/index.js
@@ -0,0 +1,124 @@
+'use strict'
+
+let mainSection = null,
+    noteElem = null,
+    currentNoteID = null,
+    noteModalInfo = null,
+    isModalOpen = false;
+
+window.addEventListener('DOMContentLoaded', () => {
+    updateNotes();
+
+    const createNoteBtn = document.querySelector('#create-note__submit'),
+          subjectInput = document.querySelector('#create-note__subject'),
+          textInput = document.querySelector('#create-note__text');
+
+    createNoteBtn.addEventListener('click', (e) => {
+        if (subjectInput.value.length == 0) {
+            subjectInput.value = 'New note';
+        }
+        new Note (
+            '',
+            subjectInput.value,
+            textInput.value,
+            currentDate()
+        ).addNote();
+        updateNotes();
+    })
+})
+
+function updateNotes () {
+    let noteBoard = document.querySelector('.note__board'),
+        currentNotes = document.querySelectorAll('.note__container');
+
+    currentNotes.forEach( elem => {
+        elem.remove();
+    })
+
+    for (let elem in noteDB) {
+        noteBoard.insertAdjacentHTML('beforeend' , 
+        new Note(
+            noteDB[elem]['id'],
+            noteDB[elem]['subject'],
+            noteDB[elem]['text'],
+            noteDB[elem]['date']
+        ).render());
+    }
+
+    showNoteInfo();
+}
+
+function showNoteInfo() {
+    noteElem = document.querySelectorAll('.note__container');
+    noteElem.forEach( elem => {
+        elem.addEventListener('click', (e) => {
+            mainSection = document.querySelector('main');
+            mainSection.insertAdjacentHTML('beforeend', noteModalElem);
+
+            isModalOpen = true;
+            document.body.style.overflow = 'hidden';
+
+            noteModalInfo = document.querySelector('.modal__container');
+
+            document.querySelector('#note__close').addEventListener('click', () => {
+                    closeModal();
+            });
+
+            document.querySelector('#note__copy').addEventListener('click', () => {
+                document.querySelector('.modal__text').select();
+                document.execCommand('copy');
+            });
+
+            document.querySelector('#note__edit').addEventListener('click', () => {
+                noteDB.forEach((elem) => {
+                    if (elem.id == currentNoteID) {
+                        noteProperties.forEach((property) => {
+                                elem[property] = noteModalInfo.querySelector(`.modal__${property}`).value;
+                                if (property == 'date') {
+                                    elem[property] = noteModalInfo.querySelector(`.modal__${property}`).innerText;
+                                }
+                        });
+                    }
+                });
+                updateNotes();
+                console.log(noteDB);
+                closeModal();
+            });
+
+            document.querySelector('#note__delete').addEventListener('click', () => {
+                noteDB.forEach((elem) => {
+                    if (elem.id == currentNoteID) {
+                        noteDB.splice(elem.id, 1);
+                        updateNotes();
+                    }
+                });
+                console.log(noteDB);
+                closeModal();
+            })
+
+            currentNoteID = e.target.closest('.note__item').querySelector('.note__id').innerText;
+            noteDB.forEach( elem => {
+                if (elem.id == currentNoteID) {
+                    noteProperties.forEach(property => {
+                        noteModalInfo.querySelector(`.modal__${property}`).value = elem[property];
+                        if (property == 'date') {
+                            noteModalInfo.querySelector(`.modal__${property}`).innerText = elem[property];
+                        }
+                    });
+                }
+            });
+        });
+    });
+}
+
+function closeModal() {
+    isModalOpen = false;
+    mainSection.removeChild(noteModalInfo);
+    document.body.style.overflow = '';
+}
+
+function currentDate() {
+    let date = new Date();
+    let result = `${date.getDate()} ${date.toLocaleString('en', { month: 'short' })} ${date.getFullYear()}`;
+    return result;
+}
---
+++ dev_data_test/js/noteDB.js
@@ -0,0 +1,22 @@
+let noteProperties = ['subject', 'text', 'date'];
+
+let noteDB = [
+    {
+        id : 0,
+        subject : 'Test0',
+        text : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, aliquam. Temporibus dolorum molestiae vitae, facere veritatis sint accusamus minima suscipit blanditiis voluptatibus praesentium illum molestias?', 
+        date : '21 Nov 2021'
+    }, 
+    {
+        id : 1,
+        subject : 'Test1',
+        text : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum, non nesciunt voluptates debitis odit vel', 
+        date : '20 Nov 2021'
+    },
+    {
+        id : 2,
+        subject : 'Test2',
+        text : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia magnam molestias accusantium, consequatur odio maxime modi rerum rem porro aspernatur repellendus exercitationem natus nam fugiat? Quibusdam praesentium hic assumenda. Totam explicabo itaque, officiis quam aliquam cupiditate reiciendis laboriosam ducimus expedita dolore. Deleniti id sunt error iusto? Assumenda in neque unde!', 
+        date : '19 Nov 2021'
+    }
+]
